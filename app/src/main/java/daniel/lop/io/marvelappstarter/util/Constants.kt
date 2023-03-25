18697c2a52193dd57f0cd63b0049d1f51dc09c43
Pedro Luiz <70184804+luizsfl@users.noreply.github.com>
2023-03-24 21:32:58 -0300

    Criando a busca
    Fontes que contem as funções para a viewModel de busca e o fragment

--- app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -5,6 +5,8 @@ object Constants {
     const val PUBLIC_KEY  = "767bf566fe6415307cd8b370998457b4"
     const val PRIVATE_KEY = "16baf1d95bdcabcef9c07c258e3fe052977e8438"
 
+    const val LAST_SEARCH_QUERY = "last_search_query"
+    const val DEFAULT_QUERY = ""
     const val APIKEY = "apikey"
     const val HASH = "hash"
     const val TS = "ts"
